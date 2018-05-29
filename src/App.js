8a5ea61f9076e88ed006484d365a924fc155901f
vigneshwar-balakrishnan <vigneshwar.balakrishnan@gmail.com>
2018-05-29 13:31:15 +0530

    react-router linked to pages

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React, { Component } from 'react';
 import Header from './components/header';
 import Footer from './components/footer';
 import Gallery from './components/pages/gallery';
-import Overview from './components/pages/overview'
+import Overview from './components/pages/overview';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header className="App-header"/>
-        <Overview />
-      
-        <Footer />
+      <Router>
+        <div className="App">
+          <Header className="App-header"/>
+          
+          <Route exact path='/gallery' component={Gallery} />
+          <Route exact path='/overview' component={Overview} />
+        
+          <Footer />
+        </div>
+      </Router>
 
-      </div>
     );
   }
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import Link from 'react-router-dom/Link';
+import '../App.css';
+
 
 class Header extends Component{
     render(){
         return(
             <header className="App-header" >
                 <h1>Gazor</h1>
+
+                <nav>
+                    <ul className="Click">
+                    <li>
+                    <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                    <Link to="/gallery">Gallery</Link>
+                
+                    </li>
+                    <li>
+                    <Link to="/overview">Overview</Link>
+                    </li>
+                    </ul>
+                </nav>
+
             </header>
 
         )
